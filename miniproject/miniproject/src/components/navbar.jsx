d0d93a702ac16077b6fb8cf32f37881591563f46
Dharti patel <156826399+Dharti-patel4@users.noreply.github.com>
2024-12-21 15:43:20 +0530

    Add files via upload

---
+++ miniproject/miniproject/src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'space-around', 
+      alignItems: 'center', 
+      padding: '10px', 
+      backgroundColor: ' rgb(29, 77, 116)', 
+      boxShadow: '0px 4px 6px rgba(239, 234, 234, 0.1)' ,
+      
+       
+    }}>
+      <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Home</Link>
+      <Link to="/Product" style={{ textDecoration: 'none', color: 'white' }}>Product</Link>
+      <Link to="/Addproduct" style={{ textDecoration: 'none', color: 'white' }}>Add Product</Link>
+      <Link to="/Login" style={{ textDecoration: 'none', color: 'white' }}>Login</Link>
+    </div>
+  );
+}
+
+export default Navbar;
